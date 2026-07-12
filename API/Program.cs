3df391aa610a8312f34926d29021989d2995165e
Fahad Mehmood <fahadmehmood009@gmail.com>
2026-07-12 15:13:12 +0500

    Setup APIs Added API project and implemented the initial customer get calls Integrated the Scalar for API testing...

---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using Core.Entities;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ISPDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ISPDbString"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Get all customers
+app.MapGet("/api/Customers", async (ISPDBContext dbContext) =>
+    await dbContext.Customers.ToListAsync());
+
+// Get a customer by ID
+app.MapGet("/api/Customers/{id:int}", async (int id, ISPDBContext dbContext) =>
+    await dbContext.Customers.FindAsync(id) is Customer customer ? Results.Ok(customer) : Results.NotFound());
+
+app.Run();
