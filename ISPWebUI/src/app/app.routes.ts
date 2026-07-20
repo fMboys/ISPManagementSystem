1ce41dad0bf0b4652b87134d6b4be4342af32d2f
Fahad Mehmood <fahadmehmood009@gmail.com>
2026-07-20 20:34:07 +0500

    Added routes and navigations for component.

--- ISPWebUI/src/app/app.routes.ts
+++ ISPWebUI/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', loadComponent: () => import('./features/home/home').then(m => m.Home)},
+    {path: 'customer', loadComponent: () => import('./features/customer').then(m => m.Customer)},
+//       {
+//     path: 'customers/:id',
+//     loadComponent: () => import('./customer-detail/customer-detail').then(m => m.CustomerDetailComponent),
+//   },
+//   {
+//     path: 'add-customer',
+//     loadComponent: () => import('./customer-form/customer-form').then(m => m.CustomerFormComponent),
+//   },
+    {path: '**', redirectTo: 'home'}
+];
--- ISPWebUI/src/app/app.ts
+++ ISPWebUI/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, effect, inject, OnInit, signal, Signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./core/nav-bar/nav-bar";
-import { Home } from "./features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, Home],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
